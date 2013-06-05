566f05d5731d4ffd2e366562c483c3f9a80bc1f0
budha <budha@budlite.hsd1.ca.comcast.net>
2013-06-05 00:55:54 -0700

    Initial Commit

---
+++ SafariLauncher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SafariLauncher
+//
+//  Created by budha on 6/5/13.
+//  Copyright (c) 2013 Bytearc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SafariLauncher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SafariLauncher
+//
+//  Created by budha on 6/5/13.
+//  Copyright (c) 2013 Bytearc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
